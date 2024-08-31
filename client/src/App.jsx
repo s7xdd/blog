dc177856ba9d0db26c24d043309fabe383aef0d2
Mohammed <kmshamil123@gmail.com>
2024-08-31 18:36:27 +0530

    Initial

---
+++ client/src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2023/11/openAI-pattern-01.jpg?resize=900,506" alt="" />
+        </div>
+        
+        <div className='texts'>
+          <h2>Investors are already valuing OpenAI at over $100B on the secondaries market</h2>
+          <p>OpenAI is in talks to raise a new round of funding at an eye-popping $100 billion-plus valuation, sources told The Wall Street Journal this week.</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2023/11/openAI-pattern-01.jpg?resize=900,506" alt="" />
+        </div>
+        
+        <div className='texts'>
+          <h2>Investors are already valuing OpenAI at over $100B on the secondaries market</h2>
+          <p>OpenAI is in talks to raise a new round of funding at an eye-popping $100 billion-plus valuation, sources told The Wall Street Journal this week.</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2023/11/openAI-pattern-01.jpg?resize=900,506" alt="" />
+        </div>
+        
+        <div className='texts'>
+          <h2>Investors are already valuing OpenAI at over $100B on the secondaries market</h2>
+          <p>OpenAI is in talks to raise a new round of funding at an eye-popping $100 billion-plus valuation, sources told The Wall Street Journal this week.</p>
+        </div>
+      </div>
+        
+ 
+
+    </main>
+  )
+}
+
+export default App
