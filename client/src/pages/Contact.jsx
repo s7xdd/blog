53343054ba7a07182eb881e76c8985152e965b03
Mohammed <kmshamil123@gmail.com>
2024-09-08 23:06:55 +0530

    Added contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import '../styles/Contact.css'
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
 
-export default Contact
+      return (
+      <div className="ContactForm">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <div className="contactForm">
+                <h1 className="pb-4 fw-bolder">CONTACT ME</h1>
+                <form id="contact-form">
+                  <div className="row formRow">
+                    <div className="col-6">
+                      <input
+                        type="text"
+                        name="name"
+                        className="form-control formInput"
+                        placeholder="Name"
+                      ></input>
+                    </div>
+                    <div className="col-6">
+                      <input
+                        type="email"
+                        name="email"
+                        className="form-control formInput"
+                        placeholder="Email address"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row formRow">
+                    <div className="col">
+                      <input
+                        type="text"
+                        name="subject"
+                        className="form-control formInput"
+                        placeholder="Subject"
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row formRow">
+                    <div className="col">
+                      <textarea
+                        rows={3}
+                        name="message"
+                        className="form-control formInput"
+                        placeholder="Message"
+                      ></textarea>
+                    </div>
+                  </div>
+
+                  <button
+                    className="submit-btn btn btn-primary"
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Contact;
